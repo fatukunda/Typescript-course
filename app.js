86eb16cd7ef3866ebecbb3430e374863600bdc5d
Frank <fatukunda@gmail.com>
2018-07-27 16:21:50 +0300

    Classes and inheritance

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 "use strict";
-//TYPES.........................................................................................................
-// type bankAccount = { money: number, deposit: (value: number) => void}
-// let bankAccount: bankAccount = {
-//     money: 2000,
-//     deposit(value: number){
-//         this.money += value;
-//     }
-// }
-// let myself: { name: string, bankAccount: bankAccount, hobbies: string[]} = {
-//     name: "Frank",
-//     bankAccount: bankAccount,
-//     hobbies: ['Swimming', 'Dancing']
-// }
-// myself.bankAccount.deposit(3000);
-// console.log(myself);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Frank",
+    bankAccount: bankAccount,
+    hobbies: ['Swimming', 'Dancing']
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
 //ES6 REVIEW .....................................................................................................
-// Exercise 1 -  Arrow Function
+//Exercise 1 -  Arrow Function
 var double = function (value) { return value * 2; };
 console.log(double(10));
 // Exercise 2 - provide some default values...
@@ -40,3 +48,72 @@ console.log(result1, result2, result3);
 var scientist = { firstName: "Will", experience: 12 };
 var firstName = scientist.firstName, experience = scientist.experience;
 console.log(firstName, experience);
+//CLASSES ---------------------------------------------------------------------------------------------------
+// Exercise 1 - TypeScript Class
+var Car = /** @class */ (function () {
+    function Car(name) {
+        this.name = name;
+        this.acceleration = 2;
+    }
+    Car.prototype.honk = function () {
+        console.log('Tooooooooooooooot!');
+    };
+    Car.prototype.accelerate = function (speed) {
+        this.acceleration + speed;
+    };
+    return Car;
+}());
+var car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(10);
+console.log(car.acceleration);
+//Inheritance ------------------------------------------------------------------------------------------
+// Exercise 2 - Two objects, based on each other ...
+var baseObject = /** @class */ (function () {
+    function baseObject(width, length) {
+        if (width === void 0) { width = 2; }
+        if (length === void 0) { length = 4; }
+        this.width = width;
+        this.length = length;
+    }
+    return baseObject;
+}());
+var rectangle = /** @class */ (function (_super) {
+    __extends(rectangle, _super);
+    function rectangle(width, length) {
+        return _super.call(this) || this;
+    }
+    rectangle.prototype.calcSize = function () {
+        return this.width * this.length;
+    };
+    return rectangle;
+}(baseObject));
+var newRectangle = new rectangle(5, 2);
+console.log(newRectangle.calcSize());
+// Exercise 3 - Getters and setters
+var Person = /** @class */ (function () {
+    function Person(_firstName) {
+        if (_firstName === void 0) { _firstName = 'Frank'; }
+        this._firstName = _firstName;
+    }
+    Object.defineProperty(Person.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._firstName = value;
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+var person = new Person();
+console.log(person.firstName);
+person.firstName = "Ma";
+console.log(person.firstName);
+person.firstName = "Maximilian";
+console.log(person.firstName);
--- app.ts
+++ app.ts
@@ -1,26 +1,26 @@
 //TYPES.........................................................................................................
-// type bankAccount = { money: number, deposit: (value: number) => void}
+type bankAccount = { money: number, deposit: (value: number) => void}
 
-// let bankAccount: bankAccount = {
-//     money: 2000,
-//     deposit(value: number){
-//         this.money += value;
-//     }
-// }
+let bankAccount: bankAccount = {
+    money: 2000,
+    deposit(value: number){
+        this.money += value;
+    }
+}
 
 
-// let myself: { name: string, bankAccount: bankAccount, hobbies: string[]} = {
-//     name: "Frank",
-//     bankAccount: bankAccount,
-//     hobbies: ['Swimming', 'Dancing']
+let myself: { name: string, bankAccount: bankAccount, hobbies: string[]} = {
+    name: "Frank",
+    bankAccount: bankAccount,
+    hobbies: ['Swimming', 'Dancing']
 
-// }
+}
 
-// myself.bankAccount.deposit(3000);
-// console.log(myself);
+myself.bankAccount.deposit(3000);
+console.log(myself);
 
 //ES6 REVIEW .....................................................................................................
-// Exercise 1 -  Arrow Function
+//Exercise 1 -  Arrow Function
 const double = (value:number) => value * 2;
 console.log(double(10));
 // Exercise 2 - provide some default values...
@@ -44,3 +44,68 @@ console.log(result1, result2, result3);
 const scientist = { firstName: "Will", experience: 12 };
 const { firstName, experience } = scientist;
 console.log(firstName, experience);
+
+//CLASSES ---------------------------------------------------------------------------------------------------
+// Exercise 1 - TypeScript Class
+class Car {
+    acceleration: number = 2;
+    constructor(public name: string){
+        
+    }
+    honk(){
+        console.log('Tooooooooooooooot!')
+    }
+    accelerate(speed: number){
+        this.acceleration + speed;
+    }
+}
+const car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(10);
+console.log(car.acceleration);
+
+//Inheritance ------------------------------------------------------------------------------------------
+
+// Exercise 2 - Two objects, based on each other ...
+class baseObject {
+    constructor(public width: number = 2, public length: number = 4) {   
+    }
+}
+
+class rectangle extends baseObject {
+    constructor(width: number, length: number) {
+        super()
+        
+    }
+    calcSize(){
+        return this.width* this.length;
+    }
+}
+const newRectangle = new rectangle(5,2);
+console.log(newRectangle.calcSize());
+
+// Exercise 3 - Getters and setters
+class Person {
+    constructor(private _firstName: string = 'Frank') {   
+    }
+    
+    get firstName() : string {
+        return this._firstName;
+    }
+    
+    set firstName(value : string) {
+        if (value.length>3) {
+            this._firstName = value
+        }
+    }
+    
+    
+}
+const person = new Person();
+console.log(person.firstName);
+person.firstName = "Ma";
+console.log(person.firstName);
+person.firstName = "Maximilian";
+console.log(person.firstName);
+

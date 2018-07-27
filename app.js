db4811180ff166f75e1dc45800b5f76f7923c9d8
Frank <fatukunda@gmail.com>
2018-07-27 13:32:35 +0300

    TypeScript types

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('It works!');
+let bankAccount = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+let myself = {
+    name: "Frank",
+    bankAccount: bankAccount,
+    hobbies: ['Swimming', 'Dancing']
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('It works!');
+type bankAccount = { money: number, deposit: (value: number) => void}
+
+let bankAccount: bankAccount = {
+    money: 2000,
+    deposit(value: number){
+        this.money += value;
+    }
+}
+
+
+let myself: { name: string, bankAccount: bankAccount, hobbies: string[]} = {
+    name: "Frank",
+    bankAccount: bankAccount,
+    hobbies: ['Swimming', 'Dancing']
+
+}
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
